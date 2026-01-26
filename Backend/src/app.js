165384285d52580a5adb4e82c5b072f3474835cf
Harsh Upadhyay <uharsh328@gmail.com>
2026-01-26 15:01:41 +0530

    Refactor app.js to include user routes and improve middleware setup

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -3,6 +3,8 @@ import { createServer } from "node:http";
 
 import { Server } from "socket.io";
 
+import { userRouter } from "./Routes/user.models.js";
+
 import mongoose from "mongoose";
 
 import cors from "cors";
@@ -11,6 +13,10 @@ const app = express();
 const server = createServer (app);
 const io = new Server (server);
 app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/api/v1/users", userRouter);
 
 app.set("port", (process.env.PORT || 8000))
 
@@ -19,9 +25,11 @@ app.get ("/home", (req,res) =>{
     return res.json({"hello": "World"})
 });
 
+
+
 const start = async () => {
     app.set("mongo_user")
-    const connectiondb = await mongoose.connect("mongodb+srv://harsh05_db_user:<WQY5auAPCihg0MPC>@clusterdb.sla0dza.mongodb.net/")
+    const connectiondb = await mongoose.connect("mongodb+srv://ritualreflection005_db_user:HarshUpadhyay0511@clustermeetconnect.hf3ri0y.mongodb.net/")
     console.log(`MONGO Connected DB Host: ${connectiondb.connection.host}`);
 
     server.listen(app.get("port"), () => {
@@ -31,3 +39,4 @@ const start = async () => {
 
 
 start();
+

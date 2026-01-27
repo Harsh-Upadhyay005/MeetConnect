8055b33e36350a7df48379af81c143da169771b3
Harsh Upadhyay <uharsh328@gmail.com>
2026-01-27 15:21:22 +0530

    Add activity routes to userRouter for managing user activities

--- Backend/src/Routes/user.models.js
+++ Backend/src/Routes/user.models.js
@@ -1,12 +1,12 @@
 import { Router } from "express";
-import { loginUser, registerUser } from "../controllers/user.controller.js";
+import { loginUser, registerUser, addToActivity, getToActivity } from "../controllers/user.controller.js";
 
 
 const userRouter = Router();
 
 userRouter.route("/login").post(loginUser);
 userRouter.route("/register").post(registerUser);
-userRouter.route("/get_to_activity ")
-userRouter.route("/add_to_activity ")
+userRouter.route("/get_all_activity").get(getToActivity);
+userRouter.route("/add_to_activity").post(addToActivity);
 
 export { userRouter };

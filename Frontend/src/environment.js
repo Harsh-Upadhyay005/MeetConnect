46cfd856b8f48911c6b4652bddc53ad8b1bd6001
Harsh Upadhyay <uharsh328@gmail.com>
2026-01-29 02:25:32 +0530

    Update environment configuration for production server URL

--- Frontend/src/environment.js
+++ Frontend/src/environment.js
@@ -1,6 +1,11 @@
-let IS_PROD = false;
+// let IS_PROD = process.env.NODE_ENV === "production";
+// const server = IS_PROD ?
+//     process.env.VITE_APP_PROD_SERVER_URL :
+//     "http://localhost:8000";
+
+let IS_PROD = true;
 const server = IS_PROD ?
-    "https://apnacollegebackend.onrender.com" :
+    "https://meetconnectbackend.onrender.com" :
     "http://localhost:8000";
 
 export default server;

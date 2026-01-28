4e78c287f026a5bf9562acb923020e2440908eca
Harsh Upadhyay <uharsh328@gmail.com>
2026-01-29 02:43:02 +0530

    Refactor environment configuration to use VITE_SERVER_URL for server URL management

--- Frontend/src/environment.js
+++ Frontend/src/environment.js
@@ -1,11 +1,3 @@
-// let IS_PROD = process.env.NODE_ENV === "production";
-// const server = IS_PROD ?
-//     process.env.VITE_APP_PROD_SERVER_URL :
-//     "http://localhost:8000";
-
-let IS_PROD = true;
-const server = IS_PROD ?
-    "https://meetconnectbackend.onrender.com" :
-    "http://localhost:8000";
+const server = import.meta.env.VITE_SERVER_URL;
 
 export default server;
